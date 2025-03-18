e99a1e4670ba1d21ae3572bb55c1958a490b9410
Santiago Gutiérrez Villegas <santiago.gutierrez@clippers.com>
2025-03-17 23:24:45 -0500

    feat: initialize project

---
+++ src/main/java/com/globant/automation/App.java
@@ -0,0 +1,13 @@
+package com.globant.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
